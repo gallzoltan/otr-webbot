b2d52b208783bc703cbe72520dd71a7deff9476b
Gáll Zoltán <gallzoltan@gmail.com>
2024-03-11 11:21:47 +0100

    fixed decision_service test

--- tests/test_decision_service.py
+++ tests/test_decision_service.py
@@ -5,10 +5,16 @@ from otr_bot.services.decision_service import DecisionService
 def service():
   return DecisionService(browser=None)
 
-def test_split_by_dots(service):
-  s = "4.1.1.; 9.2.; 9.4.;18."
-  # cat_list = [x.strip() for x in s.split(";") if x.strip()]
-  assert service._split_by_dots('4.1.1.') == ('4.', '4.1.', '4.1.1.')
-  assert service._split_by_dots('9.2.') == ('9.', '9.2.')
-  assert service._split_by_dots('9.4.') == ('9.', '9.4.')
-  assert service._split_by_dots('18.') == ('18.',)
+@pytest.mark.parametrize("cat_list, expected_result", [
+  ("4.1.1.", ('4.', '4.1.', '4.1.1.')),
+  ("9.2.", ('9.', '9.2.')),
+  ("9.4.", ('9.', '9.4.')),
+  ("18.", ('18.',)),
+])
+
+def test_split_by_dots(service, cat_list, expected_result):
+  '''
+    Test the _split_by_dots() method
+    s = "4.1.1.; 9.2.; 9.4.;18."
+  '''
+  assert service._split_by_dots(cat_list) == expected_result  
